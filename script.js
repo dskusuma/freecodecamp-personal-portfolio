e386315cf9c6bab977503293feb91a4d6444a904
dskusuma <dwisatriakusuma4@gmail.com>
2017-07-28 20:14:14 +0800

    add footer and fix some bugs

---
+++ script.js
@@ -0,0 +1,12 @@
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({target: '.navbar-fixed-top'})
+
+// // jQuery for page scrolling feature - requires jQuery Easing plugin
+// $(function() {$('body').on('click', 'a.scrollable', function(event) {
+//   var $anchor = $(this);
+//   $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
+//   event.preventDefault();
+//   });
+// });
+
+	
